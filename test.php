0d395796ccae7712ea4224ff3100488860b1e916
crazyiven <crazyiven@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-03 17:12:08 +0000

    git-svn-id: https://plugins.svn.wordpress.org/mytreasures/trunk@38057 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
 Plugin Name: myTreasures
 Plugin URI: http://www.mytreasures.de
 Description: Show your treasures (DVDs, Games, Cars & many more) in Wordpress
-Version: 0.0.997b3
+Version: 0.0.998
 Author: Marcus Jaentsch
 Author URI: http://www.crazyiven.de/
 
@@ -12,6 +12,9 @@ Author URI: http://www.crazyiven.de/
 			**************************************************************************
 			* Code starts here - NO TOUCHING! (If you don't know what you're doing!) *
 			**************************************************************************
+
+ This is just a API Test, the real version of myTreasures will follow soon!
+
 
 */
 
